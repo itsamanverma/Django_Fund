08e858ce5111b09b8f4f1ac618d57663ee3f609f
itsamanverma <amanvermame786@gmail.com>
2021-02-25 22:08:25 +0530

    change in setting.py

--- website/settings.py
+++ website/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'travello',
         'USER': 'postgres',
         'PASSWORD': 'Amankv@1234',
